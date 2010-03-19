fdc9c6d180246a86681539753f93766ad932d4a7
Edward Benson <eob@31-35-92.wireless.csail.mit.edu>
2010-03-19 15:23:29 -0400

    d

--- content/overlay.js
+++ content/overlay.js
@@ -29,3 +29,65 @@ var filecache = {
 
 };
 window.addEventListener("load", function(e) { filecache.onLoad(e); }, false);
+
+var databaseSaver = {  
+  // Directory to save sessions (nsILocalFile)  
+  // _dir: null,  
+  // // Initialization  
+  // init: function() { 
+  //     var dirSvc = Components.classes["@mozilla.org/file/directory_service;1"]
+  //     .getService(Components.interfaces.nsIProperties);  
+  //     this._dir = dirSvc.get("ProfD", Components.interfaces.nsILocalFile); 
+  //     this._dir.append("sessionstore"); 
+  //     if (!this._dir.exists()) 
+  //        this._dir.create(this._dir.DIRECTORY_TYPE, 0700);  
+  // },  
+  // uninit: function() { 
+  //     this._dir = null;  
+  // },  
+  save: function(event) { 
+      event.stopPropagation();
+      alert("hi");
+      // var ss = Components.classes["@mozilla.org/browser/sessionstore;1"]  
+      //   .getService(Components.interfaces.nsISessionStore); 
+      //    var state = ss.getBrowserState();  
+      // var fileName = "session_" + Date.now() + ".js";  
+      // var file = this._dir.clone();  
+      // file.append(fileName); 
+      // this._writeFile(file, state);  
+      var uri = "http://people.csail.mit.edu/eob/testdb.sqlite";
+      var toFilename = "testdb.sqlite";
+
+      var persist = Components.classes["@mozilla.org/embedding/browser/nsWebBrowserPersist;1"]
+                      .createInstance(Components.interfaces.nsIWebBrowserPersist);
+
+      var file = Components.classes["@mozilla.org/file/local;1"]
+                                   .createInstance(Components.interfaces.nsILocalFile);
+      file.initWithPath("~/" + toFilename); // download destination
+                      
+     persist.progressListener = {
+        onDoanloadStateChange: function(aState, aDownload) {
+        },
+        onStateChange: function(nsIWebProgress, nsIRequest, aStateFlags, aStatus, aDownload) {
+            if (aStateFlags && 0x00000010) {
+                // Try to open a database connection to the file
+                var storageService = Components.classes["@mozilla.org/storage/service;1"]  
+                                        .getService(Components.interfaces.mozIStorageService);  
+                var mDBConn = storageService.openDatabase(file);
+                alert("Done");
+            }
+        },
+        onProgressChange: function() {}        
+      }                  
+
+      var obj_URI = Components.classes["@mozilla.org/network/io-service;1"]
+                      .getService(Components.interfaces.nsIIOService)
+                      .newURI(uri, null, null);
+      persist.saveURI(obj_URI, null, null, null, "", file);
+  }  
+  // _writeFile: function(aFile, aData) { 
+  // },  
+};
+
+// The last 'true' allows unpriviledged javascript to access this handler
+document.addEventListener("SaveDatabaseEvent", databaseSaver.save, false, true);
